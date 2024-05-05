5ef03a998286e4e106c9c366f687d749aae5fc1d
IcePillow <icepillow27@gmail.com>
2024-05-05 19:26:44 -0400

    First build

--- Assets/Scripts/Parents/PantryParent.cs
+++ Assets/Scripts/Parents/PantryParent.cs
@@ -1,9 +1,7 @@
 using System.Linq;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class PantryParent : Parent
 {
